479062f1c523fa777c773f59d0744b1b86db37e0
SaltyAga <aga.ania@gmail.com>
2019-03-21 13:23:29 +0100

    pushing of current version

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var socket = require('socket.io');
+var Typo = require("typo-js");
+var dictionary = new Typo( "en_US" );
+
+//App setup
+var app = express();
+
+var server = app.listen(5000, () => {
+    console.log('Web server is running');
+});
+
+//static files
+app.use(express.static('public'));
+ 
+
+//Socket setup
+var io = socket(server);
+
+//creating an array to store all the words of one game
+var wordsArr = [];
+
+io.on ('connection', (socket) => {
+    console.log('made socket connection', socket.id);
+    socket.on('disconnect', () => {
+        wordsArr = [];
+    })
+    socket.on('game', (data) => {
+        
+        if (typeof data.word != 'undefined') {
+            if (dictionary.check(data.word)) {
+                if (wordsArr.indexOf(data.word) === -1) {
+                    wordsArr.push(data.word);
+                    io.sockets.emit('game', data);
+                } else {
+                    data.warning = 'This word has been already in use';
+                    io.sockets.emit('game', data);
+                } 
+            } else {
+                data.warning = 'Check the spelling';
+                io.sockets.emit('game', data);
+            }
+        } else {
+            data.warning = 'Uncorrect word';
+            io.sockets.emit('game', data);
+        };    
+    });
+
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', data);
+    })
+});
---
+++ public/game.js
@@ -0,0 +1,126 @@
+
+
+//Make connection
+var socket = io.connect('http://localhost:5000');
+
+//query DOM
+var output = document.getElementById('output'),
+    namePlayer = document.getElementById('player-name'),
+    word = document.getElementById('word'),
+    btn = document.getElementById('send'),
+    feedback = document.getElementById('feedback'),
+    timer = document.getElementById('timer'),
+    socketId = document.getElementById('socket-id'),
+    gameOver = document.getElementById('game-over'),
+    btnPlayAgain = document.getElementById('play-again'),
+    lastLetter = document.getElementById('last-letter');
+
+
+
+
+//scroll down
+// var element = document.getElementById("game-window");
+// element.scrollTop = element.scrollHeight;
+
+function updateScroll() {
+    var element = document.getElementById("game-window");
+    element.scrollTop = element.scrollHeight;
+    console.log(element.scrollTop);
+};
+
+
+//timer function
+var interval = 0;
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    clearInterval(interval);
+    //interval = 0;
+    interval = setInterval(function () {
+        seconds = parseInt(timer % 60, 10);
+
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = seconds + ' seconds left';
+
+        if (--timer < -1) {
+            gameOver.style.display = 'block';
+            updateScroll();
+            display.textContent = 'Time';
+            clearInterval(interval);
+        }
+    }, 1000);
+};
+
+//play again event
+btnPlayAgain.addEventListener('click', () => {
+    location.reload();
+})
+
+//Emit event
+btn.addEventListener('click', () => {
+    updateScroll();
+    if (socket.id == socketId.value && typeof socketId.value == 'undefined') {
+        word.disabled = true;
+    } else {
+        word.disabled = false;
+        if (typeof lastLetter.value != 'undefined') {
+            if (lastLetter.value == word.value[0]) {
+                socket.emit('game', {
+                    word: word.value,
+                    name: namePlayer.value,
+                    lastLetter: word.value[word.value.length - 1],
+                    socketId: socket.id
+                });
+            } else {
+                socket.emit('game', {
+                    warning: 'Uncorrect word'
+                });
+            };
+        } else {
+            socket.emit('game', {
+                word: word.value,
+                name: namePlayer.value,
+                lastLetter: word.value[word.value.length - 1],
+                socketId: socket.id
+            });
+        };
+        word.value = '';
+        
+}});
+//Emit event
+word.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        btn.click();
+    }
+});
+
+word.addEventListener('keypress', () => {
+    socket.emit('typing', namePlayer.value);
+});
+//Listen for event
+socket.on('game', (data) => {
+    
+    if (typeof data.warning != 'undefined') {
+        feedback.innerHTML = data.warning;
+    } else {
+        lastLetter.value = data.lastLetter;
+        feedback.innerHTML = "";
+        output.innerHTML += '<p><strong>' + 
+            data.name + ':</strong> ' + data.word +
+            '</p>';
+        word.value = "";
+    };
+    console.log(feedback.innerHTML);
+    if (feedback.innerHTML == '') {
+        var duration = 10,
+            display = document.querySelector('#timer');
+        startTimer(duration, display);
+    };
+});
+
+socket.on('typing', data => {
+    feedback.innerHTML = '<p><em>' + data + 
+        ' is typing a word...</em></p>'
+})
+
